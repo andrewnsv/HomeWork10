2c661c18c303de8483809e2de5ecbdc4fa9ccd53
andrewnsv <andrejnizovoj598@gmail.com>
2023-01-07 08:58:23 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+const divs = document.querySelector(".div1");
+const selectedElement = document.querySelector("#res");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const out2 = document.querySelector("#out2");
+const outSideClick = document.getElementsByTagName("body");
+
+const input = document.querySelector("#text");
+const myBtn = document.querySelector("#myBtn");
+myBtn.disabled = true;
+
+divs.addEventListener("click", (e) => {
+  e.stopPropagation();
+  const getAttr = e.target.getAttribute("div-name");
+  selectedElement.textContent = getAttr;
+});
+
+btn1.addEventListener("click", function (e) {
+  e.stopPropagation();
+  selectedElement.innerHTML = "Button 1 was clicked";
+});
+
+btn2.addEventListener("click", function (e) {
+  e.stopPropagation();
+  selectedElement.innerHTML = "Button 2 was clicked";
+});
+
+outSideClick[0].addEventListener("click", function () {
+  selectedElement.innerHTML = "";
+});
+
+input.addEventListener("input", (event) => {
+  if (event.target.value === "") {
+    myBtn.disabled = true;
+    out2.textContent = "String is empty"
+    return;
+  }
+  for (const letter of event.target.value) {
+    if (letter >= "A" && letter <= "Z" || letter >= "А" && letter <= "Я") {
+      myBtn.disabled = true;
+      out2.textContent = "It`s forbidden to enter capital letters"
+      return;
+    }
+  }
+  myBtn.disabled = false;
+});
+
+myBtn.addEventListener("click", () => {
+  out2.textContent = input.value;
+  input.value = "";
+  myBtn.disabled = true;
+});
